c2fa71ffd145079470e100c2a99fc75fea5aa4f3
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2019-09-17 17:15:03 -0300

    Efetuando Login na Administração

---
+++ inc/users.js
@@ -0,0 +1,57 @@
+var conn = require("./db");
+
+module.exports = {
+
+    render(req, res, error){
+
+        res.render("admin/login", {
+
+            body: req.body,
+            error
+            
+        });
+    },
+
+    login(email, password){
+
+        return new Promise((resolve, reject)=>{
+
+            conn.query(`
+                
+                SELECT * FROM tb_users WHERE email = ?
+
+            `, [
+                email
+            ], (err, results)=>{
+
+                if (err){
+                    
+                    reject(err);
+
+                } else{
+
+                    if (!results.length > 0){
+
+                        reject("Usuário ou senha inválidos");
+                    }else{
+
+                        let row = results[0];
+
+                        if(row.password !== password){
+                            
+                            reject("Usuário ou senha inválidos");
+
+                        }else{
+
+                            resolve(row);
+
+                        }
+
+                    }
+
+                }
+
+            });
+        });
+    }
+};
