c20b4dda6b4450dfac24df4c6d0f2eb65992cd47
Bruno M. Cerqueira <brunocerqueira@recbasvr.com.br>
2020-09-04 21:27:11 -0300

    Paginação de dados, botões anterior e próxima

--- inc/reservations.js
+++ inc/reservations.js
@@ -72,17 +72,39 @@ module.exports = {
         });
         
     },
-    getReservations(page){
+    getReservations(req){
 
-        if (!page) page = 1;
+        return new Promise((resolve, reject)=>{
 
-        let pag = new Pagination(
-            `
-                SELECT SQL_CALC_FOUND_ROWS * FROM tb_reservations ORDER BY name LIMIT ?, ?
-            `
-        );
+            let page = req.query.page;
+            let dtstart = req.query.start;
+            let dtend = req.query.end;
+            
+            if (!page) page = 1;
+    
+            let params = [];
+    
+            if (dtstart && dtend) params.push(dtstart, dtend);
+    
+            let pag = new Pagination(
+                `
+                    SELECT SQL_CALC_FOUND_ROWS *
+                    FROM tb_reservations 
+                    ${(dtstart && dtend) ? 'WHERE date BETWEEN ? AND ?' : ''}
+                    ORDER BY name LIMIT ?, ?
+                `,
+                params
+            );
+    
+            pag.getPage(page).then(data => {
 
-        return pag.getPage(page);       
+                resolve({
+                    data,
+                    links: pag.getNavigation(req.query)
+                });
+
+            });
+        });
     },
 
     delete(id){
